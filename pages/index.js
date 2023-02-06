543b6d8d69789c2998583f895c58887446a20c7b
clayreinhardt <crp2529@gmail.com>
2023-02-06 01:18:14 -0800

    lalallallalaa

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      Dylan is the man
+     <h1>Dylan is actually the man</h1>
+     <hr />
     </div>
   )
 }
